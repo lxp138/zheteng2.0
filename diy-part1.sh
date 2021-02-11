87c447208961edf90022952fbe13681c38202987
lxp138 <76868309+lxp138@users.noreply.github.com>
2021-02-11 23:30:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,11 +14,33 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/ujincn/luci-app-smartdns-compat.git package/luci-app-smartdns-compat
-git clone https://github.com/ujincn/smartdns.git package/smartdns
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+# KoolProxyR去广告插件
+git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
+# 微信推送插件
+#git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+# 京东签到插件
+pushd package/lean
+rm -rf luci-app-jd-dailybonus
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+popd
+# adguardhome插件
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+# Clash插件
+git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash
+#PassWall插件
+git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+# SmartDNS插件
+git clone https://github.com/pymumu/openwrt-smartdns package/openwrt-smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+#强制关机插件
+#git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+#自动关机插件
+git clone https://github.com/sirpdboy/luci-app-autopoweroff package/luci-app-autopoweroff
+
+# argon主题
+pushd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
+popd
+# Edge主题
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
